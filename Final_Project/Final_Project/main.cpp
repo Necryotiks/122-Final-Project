ffaecf04a0d8dec704f6da802e5e497b04aa2236
Unknown <katherine.freund@hotmail.com>
2017-04-19 11:55:20 -0700

    Linked Code from Kathy's PC

--- Final_Project/Final_Project/main.cpp
+++ Final_Project/Final_Project/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	/*sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
 
@@ -18,5 +18,5 @@ int main()
 		window.clear();
 		window.draw(shape);
 		window.display();
-	}*/
+	}
 }
