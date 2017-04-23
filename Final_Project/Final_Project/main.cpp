e4397f828dd5e8b066efeba768ec00dcd9faafa8
Elliott Villars <eman13vil@gmail.com>
2017-04-23 16:03:49 -0700

    Combat symbols

--- Final_Project/Final_Project/main.cpp
+++ Final_Project/Final_Project/main.cpp
@@ -38,27 +38,34 @@ int main()
 
 	// set up the animations for all four directions (set spritesheet and push frames)
 	Animation walkingAnimationDown;
+	Animation walkingAnimationLeft;
+	Animation walkingAnimationRight;
+	Animation walkingAnimationUp;
 	walkingAnimationDown.setSpriteSheet(texture);
 	walkingAnimationDown.addFrame(sf::IntRect(32, 0, 32, 32));
 	walkingAnimationDown.addFrame(sf::IntRect(64, 0, 32, 32));
 	walkingAnimationDown.addFrame(sf::IntRect(32, 0, 32, 32));//finish anims.
 	walkingAnimationDown.addFrame(sf::IntRect(0, 0, 32, 32));
 
-	Animation walkingAnimationLeft;
+	
 	walkingAnimationLeft.setSpriteSheet(texture);
 	walkingAnimationLeft.addFrame(sf::IntRect(32, 32, 32, 32));
+	walkingAnimationLeft.addFrame(sf::IntRect(32, 64, 32, 32));
 	walkingAnimationLeft.addFrame(sf::IntRect(64, 32, 32, 32));
-	walkingAnimationLeft.addFrame(sf::IntRect(32, 32, 32, 32));
-	walkingAnimationLeft.addFrame(sf::IntRect(0, 32, 32, 32));
+	walkingAnimationLeft.addFrame(sf::IntRect(32, 64, 32, 32));
+	walkingAnimationLeft.addFrame(sf::IntRect(32, 64, 32, 32));
+	walkingAnimationLeft.addFrame(sf::IntRect(64, 64, 32, 32));
+	walkingAnimationLeft.addFrame(sf::IntRect(32, 64, 32, 32));
+	walkingAnimationLeft.addFrame(sf::IntRect(0, 64, 32, 32));
 
-	Animation walkingAnimationRight;
+	
 	walkingAnimationRight.setSpriteSheet(texture);
 	walkingAnimationRight.addFrame(sf::IntRect(32, 64, 32, 32));
 	walkingAnimationRight.addFrame(sf::IntRect(64, 64, 32, 32));
 	walkingAnimationRight.addFrame(sf::IntRect(32, 64, 32, 32));
 	walkingAnimationRight.addFrame(sf::IntRect(0, 64, 32, 32));
 
-	Animation walkingAnimationUp;
+	
 	walkingAnimationUp.setSpriteSheet(texture);
 	walkingAnimationUp.addFrame(sf::IntRect(32, 96, 32, 32));
 	walkingAnimationUp.addFrame(sf::IntRect(64, 96, 32, 32));
