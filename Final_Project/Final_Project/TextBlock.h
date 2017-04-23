0b022a8c79b950c4d49d74a188007f349030ab7b
SgtDingleDorf <sgtdorf@gmail.com>
2017-04-22 20:23:11 -0700

    Overloaded constructor in TextBlock.h

--- Final_Project/Final_Project/TextBlock.h
+++ Final_Project/Final_Project/TextBlock.h
@@ -13,8 +13,19 @@ public:
 		this->setStyle(sf::Text::Regular);
 		this->setPosition(pos);
 	}
+	TextBlock(int size, string Label, string num, string denom, sf::Vector2f const &pos, sf::Font &font)
+	{
+		this->setCharacterSize(size);
+		this->setFont(font);
+		string contents = Label + " " + num + "/" + denom;
+		this->setString(contents);
+		this->setStyle(sf::Text::Regular);
+		this->setPosition(pos);
+	}
 		//testlines
 	//TextBlock HP(24, "Health", sf::Vector2f(32,32), font);
 	//window.draw(HP);
+	//TextBlock XP(Charsize, "Exp:", std::to_string(MJ.getCurrentXP()), std::to_string(MJ.getNextXP()), sf::Vector2f(0,Charsize), font);
+	//window.draw(XP);
 
 };
