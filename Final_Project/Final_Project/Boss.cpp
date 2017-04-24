af8a8c397b229e79af24dfb2e5cd110bef611660
Elliott Villars <eman13vil@gmail.com>
2017-04-23 17:03:52 -0700

    FIXED BOSS.CPP

--- Final_Project/Final_Project/Boss.cpp
+++ Final_Project/Final_Project/Boss.cpp
@@ -2,15 +2,13 @@
 
 
 
-Boss::Boss(string newname, int newHP, int newSTR,int newArmor) :Creature(newname, newHP, newSTR)
+Boss::Boss(string newname, int newHP, int newSTR,int newArmor, int XP) :Creature(newname, newHP, newSTR, XP)
 {
 	hp = newHP;
 	strength = newSTR;
 	name = newname;
 	armorval = newArmor;
 }
-
-
 Boss::~Boss()
 {
 }
